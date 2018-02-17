08314d77f2e9008e90eb2bbf4b944f8efcf12e3d
selvesan <dev.selvesan@gmail.com>
2018-02-17 19:50:47 +0545

    product update patch, product get all, get by id, delete

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,28 +4,30 @@ const mongoose = require('mongoose');
 const Product = require('../../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
+
+    Product.find().exec().then((docs) => {
+        res.status(200).json(docs);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
 
+/**
+ * Save product to the mongoose database.
+ * the product constant is the model instance having api to work with the database.
+ */
 router.post('/', (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
     });
-
-    //you can use a call back or a promise
-    // product.save((err,result)=>{
-    //
-    // });
-
     product
         .save()
         .then((result) => {
-        console.log(result);
-    }).catch(err => console.log(err));
+            console.log(result);
+        }).catch(err => console.log(err));
 
     res.status(200).json({
         message: 'Handling POST requests to /products',
@@ -33,28 +35,54 @@ router.post('/', (req, res, next) => {
     });
 });
 
+
+/**
+ * Get the product by unique mongoose object id passed through the uri
+ */
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        product_id: id,
-    })
+    Product.findById(id).exec().then((doc) => {
+        //if no product found null is returned.
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: 'Product Not found'
+            })
+        }
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).json({error: error});
+    });
 });
 
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        product_id: id,
-        message: 'Product Updated'
-    })
+
+    const updateData = {};
+    for (const opts in req.body) {
+        updateData[opts] = req.body[opts];
+    }
+
+    Product.update({_id: id}, {
+        $set: updateData//you will have to write $set which will be understood by mongoose.
+    }).exec().then((response) => {
+        res.status(200).json(response);
+    }).catch((error) => {
+        res.status(500).json({error: error});
+    });
+
 });
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        product_id: id,
-        message: 'Delete Updated'
-    })
+
+    Product.remove({_id: id}).exec().then((response) => {
+        res.status(200).json(response);
+    }).catch((err) => {
+        res.status(500).json({error: err});
+    });
 });
 
 module.exports = router;
--- models/product.js
+++ models/product.js
@@ -8,4 +8,13 @@ const productSchema = mongoose.Schema({
     price: Number
 });
 
+/**
+ * When you define this schema
+ * Use have by default created a rule that your table will contain id,name and price
+ * Therefore you cannot send a third property(column) to this table
+ * eg: only id,name,price is only valid if you also send for eg a view column it will be ignored.
+ * @type {Model}
+ */
+
+
 module.exports = mongoose.model('Product', productSchema);
